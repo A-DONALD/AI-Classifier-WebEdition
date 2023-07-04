dc03bd1c6f3796bfb9d738f2d0719e746ba559e8
Y-DONALD <donaldyoungoua@gmail.com>
2023-07-04 16:07:33 +0200

    add script.js

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+import webbrowser
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+
+def main():
+    url = f"http://localhost:5000"
+    webbrowser.open(url)
+    app.run(debug=False, port='5000')
