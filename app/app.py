ab9174a42d271244de9563effb52e090c7a06a1f
Y-DONALD <donaldyoungoua@gmail.com>
2023-07-07 16:47:15 +0200

    add action of button text-classification

--- app/app.py
+++ app/app.py
@@ -1,12 +1,184 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import pickle
+import os
+import re
 import webbrowser
+import nltk
+from bs4 import BeautifulSoup
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from nltk.corpus import stopwords
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
 app = Flask(__name__)
+
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+data_dir = os.path.dirname(os.path.abspath(__file__))
+dataSets_dir = os.path.join(os.path.dirname(data_dir), 'data_sets')
+model_dir = os.path.join(data_dir, 'model')
+data = []
+RandomForest_classifier = ''
+KNeighbors_classifier = ''
+multinomial_classifier = ''
+RandomForest_accuracy = 0
+KNeighbors_accuracy = 0
+multinomial_accuracy = 0
+
+# Load dictionary
+print("Loading files ", end=" ")
+for filename in os.listdir(dataSets_dir):
+    if filename.endswith('.sgm'):
+        try:
+            print("*", end=" ")
+            with open(os.path.join(dataSets_dir, filename), 'r', encoding='utf-8') as f:
+                content = f.read()
+        except UnicodeDecodeError:
+            continue
+
+        soup = BeautifulSoup(content, 'html.parser')
+        reuters = soup.findAll('reuters')
+
+        for reuter in reuters:
+            if reuter['topics'] == "YES" and reuter.topics.text != '' and reuter.body is not None:
+                data.append({'content': reuter.body.text, 'target': reuter.topics.d.text, 'lewissplit': reuter['lewissplit']})
+
+X, y = [item['content'] for item in data], [item['target'] for item in data]
+# Text preprocessing
+documents = []
+
+stemmer = WordNetLemmatizer()
+
+for sen in range(0, len(X)):
+    # Remove all the special characters
+    document = re.sub(r'\W', ' ', str(X[sen]))
+
+    # remove all single characters
+    document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
+
+    # Remove single characters from the start
+    document = re.sub(r'\^[a-zA-Z]\s+', ' ', document)
+
+    # Converting to Lowercase
+    document = document.lower()
+
+    # Lemmatization
+    document = document.split()
+
+    document = [stemmer.lemmatize(word) for word in document]
+    document = ' '.join(document)
+
+    documents.append(document)
+
+# Convert word to number: type bag of words
+vectorizer = CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+X = vectorizer.fit_transform(documents).toarray()
+
+# Find TFIDF
+tfidfconverter = TfidfTransformer()
+X = tfidfconverter.fit_transform(X).toarray()
+
+# Training and Testing Sets
+X_train, X_test, y_train, y_test = [], [], [], []
+for i, x in enumerate(X):
+    if data[i]['lewissplit'].lower() == 'train':
+        X_train.append(x)
+        y_train.append(y[i])
+    elif data[i]['lewissplit'].lower() == 'test':
+        X_test.append(x)
+        y_test.append(y[i])
+
+if os.path.exists(os.path.join(model_dir, "RandomForest_classifier.pkl"))\
+        and os.path.exists(os.path.join(model_dir, "multinomial_classifier.pkl")) \
+        and os.path.exists(os.path.join(model_dir, "KNeighbors_classifier.pkl")):
+    # Open the model
+    print(' Loading model')
+
+    with open(os.path.join(model_dir, 'multinomial_classifier.pkl'), 'rb') as training_model:
+        model = pickle.load(training_model)
+    multinomial_classifier = model
+    y_pred = multinomial_classifier.predict(X_test)
+    multinomial_accuracy = accuracy_score(y_test, y_pred)
+
+    with open(os.path.join(model_dir, 'KNeighbors_classifier.pkl'), 'rb') as training_model:
+        model = pickle.load(training_model)
+    KNeighbors_classifier = model
+    y_pred = KNeighbors_classifier.predict(X_test)
+    KNeighbors_accuracy = accuracy_score(y_test, y_pred)
+
+    with open(os.path.join(model_dir, 'RandomForest_classifier.pkl'), 'rb') as training_model:
+        model = pickle.load(training_model)
+    RandomForest_classifier = model
+    y_pred = RandomForest_classifier.predict(X_test)
+    RandomForest_accuracy = accuracy_score(y_test, y_pred)
+
+# if in model we doesn't have all models of the classifier, we will create the model of that one
+else:
+    print('Creating model')
+
+    # Multinomial
+    multinomial_classifier = MultinomialNB()
+    multinomial_classifier.fit(X_train, y_train)
+    y_pred = multinomial_classifier.predict(X_test)
+    multinomial_accuracy = accuracy_score(y_test, y_pred)
+    with open(os.path.join(model_dir, 'multinomial_classifier.pkl'), 'wb') as picklefile:
+        pickle.dump(multinomial_classifier, picklefile)
+
+    # KNeighborsClassifier
+    KNeighbors_classifier = KNeighborsClassifier(n_neighbors=5)
+    KNeighbors_classifier.fit(X_train, y_train)
+    y_pred = KNeighbors_classifier.predict(X_test)
+    KNeighbors_accuracy = accuracy_score(y_test, y_pred)
+    with open(os.path.join(model_dir, 'KNeighbors_classifier.pkl'), 'wb') as picklefile:
+        pickle.dump(KNeighbors_classifier, picklefile)
+
+    # Algorithm of random forest
+    RandomForest_classifier = RandomForestClassifier(n_estimators=1000, random_state=0)
+    RandomForest_classifier.fit(X_train, y_train)
+    y_pred = RandomForest_classifier.predict(X_test)
+    RandomForest_accuracy = accuracy_score(y_test, y_pred)
+    with open(os.path.join(model_dir, 'RandomForest_classifier.pkl'), 'wb') as picklefile:
+        pickle.dump(RandomForest_classifier, picklefile)
+
+
+def preprocess_text(text):
+    from nltk.stem import WordNetLemmatizer
+
+    stemmer = WordNetLemmatizer()
+
+    document = re.sub(r'\W', ' ', text)
+    document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
+    document = re.sub(r'\^[a-zA-Z]\s+', ' ', document)
+    document = document.lower()
+    document = document.split()
+    document = [stemmer.lemmatize(word) for word in document]
+    document = ' '.join(document)
+
+    return document
 
 
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+
+@app.route('/txt-classify', methods=['POST'])
+def classify_text():
+    text = request.form.get('text')
+    preprocessed_text = preprocess_text(text)
+    features = vectorizer.transform([preprocessed_text]).toarray()
+    randomforest = RandomForest_classifier.predict(features)
+    kneighbors = KNeighbors_classifier.predict(features)
+    multinomial = multinomial_classifier.predict(features)
+    words = [randomforest[0], kneighbors[0], multinomial[0]]
+    print(words)
+
+    return jsonify(words=words)
 
 
 def main():
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,51 +1,90 @@
-const dropZone = document.getElementById('dropZone');
-const uploadBtn = document.getElementById('upload-button');
-const fileInput = document.getElementById('fileInput');
+let randomForestLabel;
+let kNeighborsLabel;
+let multinomialLabel;
+
+document.addEventListener('DOMContentLoaded', function() {
+    const textClassificationBtn = document.getElementById('text-classification');
+    const textInput = document.getElementById('textInput');
+    const resultDiv = document.getElementById('result-div');
 
 
-let d = new DataTransfer();
-// Handle uploaded files
-function handleFiles(files) {
-      let fileFound = false;
-      for (const file of files) {
-        for(const f of d.files){
-          if(file.name == f.name){
-            fileFound = true;
-            break;
-          }
-        }
-        if(fileFound) continue;
-        const div = document.createElement('div');
-        const li = document.createElement('li');
-        div.textContent = file.name;
-        const deleteBtn = document.createElement('button');
-        deleteBtn.textContent = 'X';
-        deleteBtn.classList.add('remove-btn');
-        deleteBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        li.remove(); // Remove the file from the file list
-        removeFileFromFileList(div.textContent.slice(0,-3));
-        });
+    textClassificationBtn.addEventListener('click', function() {
+        const text = textInput.value;
 
-        //Save in files list variable
-        d.items.add(file);
-      }
-      //console.log(d.files);
-      fileInput.files = d.files;
-      //console.log(d.items);
+        // Effectuer une requête AJAX vers la route /classify pour obtenir les mots
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', '/txt-classify');
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                const words = response.words;
+                randomForestLabel = words[0];
+                kNeighborsLabel = words[1];
+                multinomialLabel = words[2];
+                resultDiv.innerHTML = `
+                    <p>${words[0]}</p>
+                    <p>${words[1]}</p>
+                    <p>${words[2]}</p>
+                `;
 
+            }
+        };
+        xhr.send('text=' + encodeURIComponent(text));
+    });
+});
+
+let randomForestAccuracy = 85.5;
+let kNeighborsAccuracy = 83.3;
+let multinomialAccuracy = 79.8;
+
+let xValues, yValues;
+
+// config the different element of the diagram
+if (randomForestLabel === kNeighborsLabel && kNeighborsLabel === multinomialLabel) {
+  xValues = [randomForestLabel];
+  yValues = [100];
+} else if (randomForestLabel === kNeighborsLabel) {
+  xValues = [randomForestLabel, multinomialLabel];
+  yValues = [randomForestAccuracy+kNeighborsAccuracy, multinomialAccuracy];
+} else if (kNeighborsLabel === multinomialLabel) {
+  xValues = [kNeighborsLabel, randomForestLabel];
+  yValues = [kNeighborsAccuracy+multinomialAccuracy, randomForestAccuracy];
+} else if (randomForestLabel === multinomialLabel) {
+  xValues = [randomForestLabel, kNeighborsLabel];
+  yValues = [randomForestAccuracy+multinomialAccuracy, kNeighborsAccuracy];
+} else {
+  xValues = [randomForestLabel, kNeighborsLabel, multinomialLabel];
+  yValues = [randomForestAccuracy, kNeighborsAccuracy, multinomialAccuracy];
 }
-function removeFileFromFileList(filename) {
-  const dt = new DataTransfer();
-  //const input = document.getElementById('fileInput')
-  const files = d.files;
 
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i]
-    if (file.name != filename){
-      dt.items.add(file) // here you exclude the file. thus removing it.
+// choose the color of the different element of the diagram
+const barColors = [
+  "#87ceeb",
+  "#000080",
+  "#6495ed"
+];
+
+// create a diagram to print out the solution with using chart.js
+new Chart("myChart", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Label of the text"
     }
   }
-  d = dt // Update the list
-  fileInput.files = d.files;
-}
+});
+
+// button to change the color of the background - change theme
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("light")
+})
